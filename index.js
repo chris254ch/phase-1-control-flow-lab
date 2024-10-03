dbb925964a37ea3565f856129864fdbd9e9a3074
chrispas mukaria <chrismuriungi3@gmail.com>
2024-10-03 10:20:37 +0300

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+if (someValue <= 400) {
+  return 'This one is on me!'; // Free ride for distances less than or equal to 400 feet
+} else if (someValue > 400 && someValue <= 2000) {
+  return 'That will be twenty bucks.'; // Charge for distances between 400 and 2000 feet
+} else if (someValue > 2000 && someValue <= 2500) {
+  return 'I will gladly take your thirty bucks.'; // Flat fare for distances between 2000 and 2500 feet
+} else {
+  return 'No can do.'; // No rides over 2500 feet
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Function to return a response based on the city
+function ternaryCheckCity(city) {
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; // Ternary operator to check if the city is NYC
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function to return a response based on the tip amount
+function switchOnCharmFromTip(tip) {
+switch (tip) {
+  case 'generous':
+    return 'Thank you so much.';
+  case 'not as generous':
+    return 'Thank you.';
+  case 'cheap':
+    return 'Bye.';
+  default:
+    return 'Bye.'; // Default case for any other tip
+}
 }
